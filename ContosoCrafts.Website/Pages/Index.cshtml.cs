b70bb596ac887a9c6baf30d4ecefe8b0a9f6f2c3
Henrique Salvan <henriquesalvan@gmail.com>
2021-06-01 21:40:56 -0300

    Feito listagem de produtos acontecer. Estou em dúvida quanto a essas pastas "bin" e "obj", talvez colocar isso no .gitignore?!?!

--- ContosoCrafts.Website/Pages/Index.cshtml.cs
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,14 +13,18 @@ namespace ContosoCrafts.Website.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly JsonFileProductService _productService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         public void OnGet()
         {
+            Products = _productService.GetProducts();
         }
     }
 }
